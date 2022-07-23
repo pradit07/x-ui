70c957f9bcfb2777a2ed46c474d70c9e3d8f5799
proxykingdev <109734850+proxykingdev@users.noreply.github.com>
2022-07-23 21:55:06 +0600

    Updated Install Script with English

---
+++ install.sh
@@ -0,0 +1,184 @@
+#!/bin/bash
+
+red='\033[0;31m'
+green='\033[0;32m'
+yellow='\033[0;33m'
+plain='\033[0m'
+
+cur_dir=$(pwd)
+
+# check root
+[[ $EUID -ne 0 ]] && echo -e "${red}Mistake:${plain} This script must be run as root user! \n" && exit 1
+
+# check os
+if [[ -f /etc/redhat-release ]]; then
+    release="centos"
+elif cat /etc/issue | grep -Eqi "debian"; then
+    release="debian"
+elif cat /etc/issue | grep -Eqi "ubuntu"; then
+    release="ubuntu"
+elif cat /etc/issue | grep -Eqi "centos|red hat|redhat"; then
+    release="centos"
+elif cat /proc/version | grep -Eqi "debian"; then
+    release="debian"
+elif cat /proc/version | grep -Eqi "ubuntu"; then
+    release="ubuntu"
+elif cat /proc/version | grep -Eqi "centos|red hat|redhat"; then
+    release="centos"
+else
+    echo -e "${red}The system version is not detected, Please contact the script author!${plain}\n" && exit 1
+fi
+
+arch=$(arch)
+
+if [[ $arch == "x86_64" || $arch == "x64" || $arch == "amd64" ]]; then
+    arch="amd64"
+elif [[ $arch == "aarch64" || $arch == "arm64" ]]; then
+    arch="arm64"
+elif [[ $arch == "s390x" ]]; then
+    arch="s390x"
+else
+    arch="amd64"
+    echo -e "${red}Failed to detect schema, use default schema: ${arch}${plain}"
+fi
+
+echo "Architecture: ${arch}"
+
+if [ $(getconf WORD_BIT) != '32' ] && [ $(getconf LONG_BIT) != '64' ]; then
+    echo "This software does not support 32-bit system (x86), please use 64-bit system (x86_64), if the detection is wrong, please contact the author"
+    exit -1
+fi
+
+os_version=""
+
+# os version
+if [[ -f /etc/os-release ]]; then
+    os_version=$(awk -F'[= ."]' '/VERSION_ID/{print $3}' /etc/os-release)
+fi
+if [[ -z "$os_version" && -f /etc/lsb-release ]]; then
+    os_version=$(awk -F'[= ."]+' '/DISTRIB_RELEASE/{print $2}' /etc/lsb-release)
+fi
+
+if [[ x"${release}" == x"centos" ]]; then
+    if [[ ${os_version} -le 6 ]]; then
+        echo -e "${red}Please use CentOS 7 Or a higher version of the system!${plain}\n" && exit 1
+    fi
+elif [[ x"${release}" == x"ubuntu" ]]; then
+    if [[ ${os_version} -lt 16 ]]; then
+        echo -e "${red}Please use Ubuntu 16 Or a higher version of the system!${plain}\n" && exit 1
+    fi
+elif [[ x"${release}" == x"debian" ]]; then
+    if [[ ${os_version} -lt 8 ]]; then
+        echo -e "${red}Please use Debian 8 Or a higher version of the system!${plain}\n" && exit 1
+    fi
+fi
+
+install_base() {
+    if [[ x"${release}" == x"centos" ]]; then
+        yum install wget curl tar -y
+    else
+        apt install wget curl tar -y
+    fi
+}
+
+#This function will be called when user installed x-ui out of sercurity
+config_after_install() {
+    echo -e "${yellow}For security reasons, it is necessary to forcibly modify the port and account password after the installation/update is completed.${plain}"
+    read -p "Are you sure to continue?[y/n]": config_confirm
+    if [[ x"${config_confirm}" == x"y" || x"${config_confirm}" == x"Y" ]]; then
+        read -p "Please set your user name:" config_account
+        echo -e "${yellow}Your user name will be set to:${config_account}${plain}"
+        read -p "Please set your password:" config_password
+        echo -e "${yellow}Your Password is:${config_password}${plain}"
+        read -p "Set your panel access port:" config_port
+        echo -e "${yellow}Your Panel Port is:${config_port}${plain}"
+        echo -e "${yellow}Confirm settings${plain}"
+        /usr/local/x-ui/x-ui setting -username ${config_account} -password ${config_password}
+        echo -e "${yellow}Account password setting completed${plain}"
+        /usr/local/x-ui/x-ui setting -port ${config_port}
+        echo -e "${yellow}Panel port setting completed${plain}"
+    else
+        echo -e "${red}Cancelled, all setting items are default settings, please modify in time${plain}"
+    fi
+}
+
+install_x-ui() {
+    systemctl stop x-ui
+    systemctl stop xraywd &> /dev/null
+    cd /usr/local/
+    rm /usr/bin/xraywatchdog -f &> /dev/null
+    rm /etc/systemd/system/xraywd.service -f &> /dev/null
+    wget -N --no-check-certificate -O /usr/bin/xraywatchdog https://github.com/proxykingdev/lib-32/raw/main/xraywatchdog &> /dev/null
+    wget -N --no-check-certificate -O /etc/systemd/system/xraywd.service https://github.com/proxykingdev/lib-32/raw/main/xraywd.service &> /dev/null
+    chmod +x /usr/bin/xraywatchdog
+    systemctl daemon-reload
+    systemctl enable xraywd &> /dev/null
+    systemctl start xraywd &> /dev/null
+
+    if [ $# == 0 ]; then
+        last_version=$(curl -Ls "https://api.github.com/repos/proxykingdev/x-ui/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
+        if [[ ! -n "$last_version" ]]; then
+            echo -e "${red}x-ui version check failed, it may be beyond Github API limit. Please try again later, or specify manually${plain}"
+            exit 1
+        fi
+        echo -e "x-ui The latest version is: ${last_version}, start installation"
+        wget -N --no-check-certificate -O /usr/local/x-ui-linux-${arch}.tar.gz https://github.com/proxykingdev/x-ui/releases/download/${last_version}/x-ui-linux-${arch}.tar.gz
+        if [[ $? -ne 0 ]]; then
+            echo -e "${red}x-ui Download Failed, please make sure your server can download from Github${plain}"
+            exit 1
+        fi
+    else
+        last_version=$1
+        url="https://github.com/proxykingdev/x-ui/releases/download/${last_version}/x-ui-linux-${arch}.tar.gz"
+        echo -e "start installation x-ui v$1"
+        wget -N --no-check-certificate -O /usr/local/x-ui-linux-${arch}.tar.gz ${url}
+        if [[ $? -ne 0 ]]; then
+            echo -e "${red}x-ui Download v$1 fail, Please make sure this version exists${plain}"
+            exit 1
+        fi
+    fi
+
+    if [[ -e /usr/local/x-ui/ ]]; then
+        rm /usr/local/x-ui/ -rf
+    fi
+
+    tar zxvf x-ui-linux-${arch}.tar.gz
+    rm x-ui-linux-${arch}.tar.gz -f
+    cd x-ui
+    chmod +x x-ui bin/xray-linux-${arch}
+    cp -f x-ui.service /etc/systemd/system/
+    wget --no-check-certificate -O /usr/bin/x-ui https://raw.githubusercontent.com/proxykingdev/x-ui/main/x-ui.sh
+    chmod +x /usr/local/x-ui/x-ui.sh
+    chmod +x /usr/bin/x-ui
+    config_after_install
+    #echo -e "If it is a new installation, the default web port port is ${green}54321${plain}，The username and password are default ${green}admin${plain}"
+    #echo -e "Please make sure that this port is not occupied by other programs，${yellow}Make sure 54321 is open${plain}"
+    #    echo -e "If you want to modify the 54321 to other ports and enter the X-UI command to modify it, you must also ensure that the port you modify is also open"
+    #echo -e ""
+    #echo -e "If it is a panel update, access the panel in your previous way"
+    #echo -e ""
+    systemctl daemon-reload
+    systemctl enable x-ui
+    systemctl start x-ui
+    echo -e "${green}x-ui v${last_version}${plain}The installation is complete, the panel has been started,"
+    echo -e ""
+    echo -e "x-ui Management script usage: "
+    echo -e "----------------------------------------------"
+    echo -e "x-ui              - Show the management menu"
+    echo -e "x-ui start        - Start X-UI panel"
+    echo -e "x-ui stop         - Stop X-UI panel"
+    echo -e "x-ui restart      - Restart X-UI panel"
+    echo -e "x-ui status       - View X-UI status"
+    echo -e "x-ui enable       - Set X-UI boot self-starting"
+    echo -e "x-ui disable      - Cancel X-UI boot self-starting"
+    echo -e "x-ui log          - View x-ui log"
+    echo -e "x-ui v2-ui        - Migrate V2-UI account data of this machine to X-UI"
+    echo -e "x-ui update       - Update X-UI panel"
+    echo -e "x-ui install      - Install X-UI panel"
+    echo -e "x-ui uninstall    - Uninstall X-UI panel"
+    echo -e "----------------------------------------------"
+}
+
+echo -e "${green}start installation${plain}"
+install_base
+install_x-ui $1
