09d751fad14d49b503d156cf3db94fd66c0be6b3
Proxy King <roypranto39@gmail.com>
2022-07-23 18:06:50 +0600

    first commit

---
+++ x-ui.sh
@@ -0,0 +1,630 @@
+#!/bin/bash
+
+red='\033[0;31m'
+green='\033[0;32m'
+yellow='\033[0;33m'
+plain='\033[0m'
+
+#Add some basic function here
+function LOGD() {
+    echo -e "${yellow}[DEG] $* ${plain}"
+}
+
+function LOGE() {
+    echo -e "${red}[ERR] $* ${plain}"
+}
+
+function LOGI() {
+    echo -e "${green}[INF] $* ${plain}"
+}
+# check root
+[[ $EUID -ne 0 ]] && LOGE "Error: You must run this script as root!\n" && exit 1
+
+# check os
+if [[ -f /etc/redhat-release ]]; then
+    release="centos"
+elif cat /etc/issue | grep -Eqi "debian"; then
+    release="debian"
+elif cat /etc/issue | grep -Eqi "ubuntu"; then
+    release="ubuntu"
+elif cat /etc/issue | grep -Eqi "centos|red hat|redhat"; then
+    release="centos"
+elif cat /proc/version | grep -Eqi "debian"; then
+    release="debian"
+elif cat /proc/version | grep -Eqi "ubuntu"; then
+    release="ubuntu"
+elif cat /proc/version | grep -Eqi "centos|red hat|redhat"; then
+    release="centos"
+else
+    LOGE "If the system version is not detected, please contact @redstone22 on TG!\n" && exit 1
+fi
+
+os_version=""
+
+# os version
+if [[ -f /etc/os-release ]]; then
+    os_version=$(awk -F'[= ."]' '/VERSION_ID/{print $3}' /etc/os-release)
+fi
+if [[ -z "$os_version" && -f /etc/lsb-release ]]; then
+    os_version=$(awk -F'[= ."]+' '/DISTRIB_RELEASE/{print $2}' /etc/lsb-release)
+fi
+
+if [[ x"${release}" == x"centos" ]]; then
+    if [[ ${os_version} -le 6 ]]; then
+        LOGE "please use CentOS 7 Or a higher version of the system!\n" && exit 1
+    fi
+elif [[ x"${release}" == x"ubuntu" ]]; then
+    if [[ ${os_version} -lt 16 ]]; then
+        LOGE "please use Ubuntu 16 Or a higher version of the system!\n" && exit 1
+    fi
+elif [[ x"${release}" == x"debian" ]]; then
+    if [[ ${os_version} -lt 8 ]]; then
+        LOGE "please use Debian 8 Or a higher version of the system!\n" && exit 1
+    fi
+fi
+
+confirm() {
+    if [[ $# > 1 ]]; then
+        echo && read -p "$1 [default$2]: " temp
+        if [[ x"${temp}" == x"" ]]; then
+            temp=$2
+        fi
+    else
+        read -p "$1 [y/n]: " temp
+    fi
+    if [[ x"${temp}" == x"y" || x"${temp}" == x"Y" ]]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+confirm_restart() {
+    confirm "Whether to restart the panel, restart the panel will restart XRAY" "y"
+    if [[ $? == 0 ]]; then
+        restart
+    else
+        show_menu
+    fi
+}
+
+before_show_menu() {
+    echo && echo -n -e "${yellow}Press enter to return to the main menu: ${plain}" && read temp
+    show_menu
+}
+
+install() {
+    bash <(curl -Ls https://raw.githubusercontent.com/proxykingdev/x-ui/master/install.sh)
+    if [[ $? == 0 ]]; then
+        if [[ $# == 0 ]]; then
+            start
+        else
+            start 0
+        fi
+    fi
+}
+
+update() {
+    confirm "This function will reinstall the latest version. The data will not be lost. Do you continue?" "n"
+    if [[ $? != 0 ]]; then
+        LOGE "Cancelled"
+        if [[ $# == 0 ]]; then
+            before_show_menu
+        fi
+        return 0
+    fi
+    bash <(curl -Ls https://raw.githubusercontent.com/proxykingdev/x-ui/master/install.sh)
+    if [[ $? == 0 ]]; then
+        LOGI "Update is completed, the panel has been automatically restarted "
+        exit 0
+    fi
+}
+
+uninstall() {
+    confirm "Are you sure to uninstall the panel and Xray?" "n"
+    if [[ $? != 0 ]]; then
+        if [[ $# == 0 ]]; then
+            show_menu
+        fi
+        return 0
+    fi
+    systemctl stop x-ui
+    systemctl disable x-ui
+    rm /etc/systemd/system/x-ui.service -f
+    systemctl daemon-reload
+    systemctl reset-failed
+    rm /etc/x-ui/ -rf
+    rm /usr/local/x-ui/ -rf
+
+    echo ""
+    echo -e "If you want to delete this script successfully, exit the script and run ${green}rm /usr/bin/x-ui -f${plain} Delete"
+    echo ""
+
+    if [[ $# == 0 ]]; then
+        before_show_menu
+    fi
+}
+
+reset_user() {
+    confirm "Are you sure to reset? Username and Password will be admin" "n"
+    if [[ $? != 0 ]]; then
+        if [[ $# == 0 ]]; then
+            show_menu
+        fi
+        return 0
+    fi
+    /usr/local/x-ui/x-ui setting -username admin -password admin
+    echo -e "Username and password have been reset to ${green}admin${plain}!Please restart the panel now"
+    confirm_restart
+}
+
+reset_config() {
+    confirm "Are you sure to reset all the panel settings, the account data will not be lost, the username and password will not change" "n"
+    if [[ $? != 0 ]]; then
+        if [[ $# == 0 ]]; then
+            show_menu
+        fi
+        return 0
+    fi
+    /usr/local/x-ui/x-ui setting -reset
+    echo -e "All panel settings have been reset to the default value. Now please restart the panel and use the default ${green}54321${plain} Port access panel"
+    confirm_restart
+}
+
+check_config() {
+    info=$(/usr/local/x-ui/x-ui setting -show true)
+    if [[ $? != 0 ]]; then
+        LOGE "get current settings error,please check logs"
+        show_menu
+    fi
+    LOGI "${info}"
+}
+
+set_port() {
+    echo && echo -n -e "Enter Panel Port[1-65535]: " && read port
+    if [[ -z "${port}" ]]; then
+        LOGD "Cancelled"
+        before_show_menu
+    else
+        /usr/local/x-ui/x-ui setting -port ${port}
+        echo -e "After the setting new port, please restart the panel and use the newly set port ${green}${port}${plain} Access panel"
+        confirm_restart
+    fi
+}
+
+start() {
+    check_status
+    if [[ $? == 0 ]]; then
+        echo ""
+        LOGI "The panel is running, no need to start again, if you need to restart, please choose to restart"
+    else
+        systemctl start x-ui
+        sleep 2
+        check_status
+        if [[ $? == 0 ]]; then
+            LOGI "x-ui Successfully start"
+        else
+            LOGE "The panel failed to start, maybe because the startup time exceeded two seconds, please check the log information later"
+        fi
+    fi
+
+    if [[ $# == 0 ]]; then
+        before_show_menu
+    fi
+}
+
+stop() {
+    check_status
+    if [[ $? == 1 ]]; then
+        echo ""
+        LOGI "The panel has stopped, no need to stop again"
+    else
+        systemctl stop x-ui
+        sleep 2
+        check_status
+        if [[ $? == 1 ]]; then
+            LOGI "x-ui and xray stop success"
+        else
+            LOGE "The panel stops failing, maybe because the stop time exceeds two seconds, please check the log information later"
+        fi
+    fi
+
+    if [[ $# == 0 ]]; then
+        before_show_menu
+    fi
+}
+
+restart() {
+    systemctl restart x-ui
+    sleep 2
+    check_status
+    if [[ $? == 0 ]]; then
+        LOGI "x-ui and xray Restart success"
+    else
+        LOGE "The panel failed to restart, maybe because the startup time exceeded two seconds, please check the log information later"
+    fi
+    if [[ $# == 0 ]]; then
+        before_show_menu
+    fi
+}
+
+status() {
+    systemctl status x-ui -l
+    if [[ $# == 0 ]]; then
+        before_show_menu
+    fi
+}
+
+enable() {
+    systemctl enable x-ui
+    if [[ $? == 0 ]]; then
+        LOGI "x-ui Enable success"
+    else
+        LOGE "x-ui Enable failure"
+    fi
+
+    if [[ $# == 0 ]]; then
+        before_show_menu
+    fi
+}
+
+disable() {
+    systemctl disable x-ui
+    if [[ $? == 0 ]]; then
+        LOGI "x-ui Disable success"
+    else
+        LOGE "x-ui Disable failure"
+    fi
+
+    if [[ $# == 0 ]]; then
+        before_show_menu
+    fi
+}
+
+show_log() {
+    journalctl -u x-ui.service -e --no-pager -f
+    if [[ $# == 0 ]]; then
+        before_show_menu
+    fi
+}
+
+migrate_v2_ui() {
+    /usr/local/x-ui/x-ui v2-ui
+
+    before_show_menu
+}
+
+install_bbr() {
+    # temporary workaround for installing bbr
+    bash <(curl -L -s https://raw.githubusercontent.com/teddysun/across/master/bbr.sh)
+    echo ""
+    before_show_menu
+}
+
+update_shell() {
+    wget -O /usr/bin/x-ui -N --no-check-certificate https://github.com/proxykingdev/x-ui/raw/master/x-ui.sh
+    if [[ $? != 0 ]]; then
+        echo ""
+        LOGE "Update download failed, please check whether the machine can connect to Github"
+        before_show_menu
+    else
+        chmod +x /usr/bin/x-ui
+        LOGI "Update success. Please re-run the script" && exit 0
+    fi
+}
+
+# 0: running, 1: not running, 2: not installed
+check_status() {
+    if [[ ! -f /etc/systemd/system/x-ui.service ]]; then
+        return 2
+    fi
+    temp=$(systemctl status x-ui | grep Active | awk '{print $3}' | cut -d "(" -f2 | cut -d ")" -f1)
+    if [[ x"${temp}" == x"running" ]]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+check_enabled() {
+    temp=$(systemctl is-enabled x-ui)
+    if [[ x"${temp}" == x"enabled" ]]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+check_uninstall() {
+    check_status
+    if [[ $? != 2 ]]; then
+        echo ""
+        LOGE "The panel has been installed, please do not install it again"
+        if [[ $# == 0 ]]; then
+            before_show_menu
+        fi
+        return 1
+    else
+        return 0
+    fi
+}
+
+check_install() {
+    check_status
+    if [[ $? == 2 ]]; then
+        echo ""
+        LOGE "Please install the panel first"
+        if [[ $# == 0 ]]; then
+            before_show_menu
+        fi
+        return 1
+    else
+        return 0
+    fi
+}
+
+show_status() {
+    check_status
+    case $? in
+    0)
+        echo -e "Panel state: ${green}Runing${plain}"
+        show_enable_status
+        ;;
+    1)
+        echo -e "Panel state: ${yellow}Not running${plain}"
+        show_enable_status
+        ;;
+    2)
+        echo -e "Panel state: ${red}Not Installed${plain}"
+        ;;
+    esac
+    show_xray_status
+}
+
+show_enable_status() {
+    check_enabled
+    if [[ $? == 0 ]]; then
+        echo -e "Whether to start with boot: ${green}Yes${plain}"
+    else
+        echo -e "Whether to start with boot: ${red}no${plain}"
+    fi
+}
+
+check_xray_status() {
+    count=$(ps -ef | grep "xray-linux" | grep -v "grep" | wc -l)
+    if [[ count -ne 0 ]]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+show_xray_status() {
+    check_xray_status
+    if [[ $? == 0 ]]; then
+        echo -e "xray Status: ${green}Runing${plain}"
+    else
+        echo -e "xray Status: ${red}Not running${plain}"
+    fi
+}
+
+ssl_cert_issue() {
+    echo -E ""
+    LOGD "******Instructions for use******"
+    LOGI "This script will use an ACME script application certificate, and it must be installed when using:"
+    LOGI "1.Cloudflare registered mailbox"
+    LOGI "2.Cloudflare Global API Key"
+    LOGI "3.The domain name of the current server on Cloudflare"
+    LOGI "4.The default installation path of this script application certificate is /root/cert directory"
+    confirm "I have confirmed the above content[y/n]" "y"
+    if [ $? -eq 0 ]; then
+        cd ~
+        LOGI "Install the ACME script"
+        curl https://get.acme.sh | sh
+        if [ $? -ne 0 ]; then
+            LOGE "Failure to install ACME script"
+            exit 1
+        fi
+        CF_Domain=""
+        CF_GlobalKey=""
+        CF_AccountEmail=""
+        certPath=/root/cert
+        if [ ! -d "$certPath" ]; then
+            mkdir $certPath
+        else
+            rm -rf $certPath
+            mkdir $certPath
+        fi
+        LOGD "Please set the domain name:"
+        read -p "Input your domain here:" CF_Domain
+        LOGD "Your domain name is set to:${CF_Domain}"
+        LOGD "Please set the API key:"
+        read -p "Input your key here:" CF_GlobalKey
+        LOGD "Your API key is:${CF_GlobalKey}"
+        LOGD "Please set up a registered mailbox:"
+        read -p "Input your email here:" CF_AccountEmail
+        LOGD "Your registered mailbox is:${CF_AccountEmail}"
+        ~/.acme.sh/acme.sh --set-default-ca --server letsencrypt
+        if [ $? -ne 0 ]; then
+            LOGE "Modify the default CA failed, the script exits"
+            exit 1
+        fi
+        export CF_Key="${CF_GlobalKey}"
+        export CF_Email=${CF_AccountEmail}
+        ~/.acme.sh/acme.sh --issue --dns dns_cf -d ${CF_Domain} -d *.${CF_Domain} --log
+        if [ $? -ne 0 ]; then
+            LOGE "Certificate Pull failed, the script exit"
+            exit 1
+        else
+            LOGI "The certificate was successfully issued, in the installation ..."
+        fi
+        ~/.acme.sh/acme.sh --installcert -d ${CF_Domain} -d *.${CF_Domain} --ca-file /root/cert/ca.cer \
+        --cert-file /root/cert/${CF_Domain}.cer --key-file /root/cert/${CF_Domain}.key \
+        --fullchain-file /root/cert/fullchain.cer
+        if [ $? -ne 0 ]; then
+            LOGE "Certificate installation failed, the script exits"
+            exit 1
+        else
+            LOGI "The certificate installation is successful, and the automatic update is turned on..."
+        fi
+        ~/.acme.sh/acme.sh --upgrade --auto-upgrade
+        if [ $? -ne 0 ]; then
+            LOGE "Automatic update settings fail, script exits"
+            ls -lah cert
+            chmod 755 $certPath
+            exit 1
+        else
+            LOGI "The certificate has been installed and the automatic update has been opened. The specific information is as follows"
+            ls -lah cert
+            chmod 755 $certPath
+        fi
+    else
+        show_menu
+    fi
+}
+
+show_usage() {
+    echo "x-ui Management script usage: "
+    echo "------------------------------------------"
+    echo "x-ui              - Show the management menu"
+    echo "x-ui start        - start up x-ui panel"
+    echo "x-ui stop         - stop x-ui panel"
+    echo "x-ui restart      - restart x-ui panel"
+    echo "x-ui status       - view x-ui status"
+    echo "x-ui enable       - enable x-ui service"
+    echo "x-ui disable      - disable x-ui service"
+    echo "x-ui log          - Check x-ui log"
+    echo "x-ui v2-ui        - switch v2-ui to x-ui"
+    echo "x-ui update       - update x-ui panel"
+    echo "x-ui install      - install x-ui panel"
+    echo "x-ui uninstall    - uninstall x-ui panel"
+    echo "------------------------------------------"
+}
+
+show_menu() {
+    echo -e "
+  ${green}x-ui Panel management script${plain}
+  ${green}0.${plain} Exit script
+————————————————
+  ${green}1.${plain} Install x-ui
+  ${green}2.${plain} Reinstall x-ui
+  ${green}3.${plain} Uninstalled x-ui
+————————————————
+  ${green}4.${plain} Reset the username password
+  ${green}5.${plain} Reset panel settings
+  ${green}6.${plain} Set the panel port
+  ${green}7.${plain} Check the front panel settings
+————————————————
+  ${green}8.${plain} Start x-ui
+  ${green}9.${plain} Stop x-ui
+  ${green}10.${plain} Restartx-ui
+  ${green}11.${plain} Check x-ui status
+  ${green}12.${plain} Check x-ui Log
+————————————————
+  ${green}13.${plain} Set x-ui auto start at boot
+  ${green}14.${plain} Stop x-ui auto start at boot
+————————————————
+  ${green}15.${plain} A key installation bbr (The latest kernel)
+  ${green}16.${plain} One -click application SSL certificate(Acme Certbot)
+ "
+    show_status
+    echo && read -p "Please enter the selection [0-16]: " num
+
+    case "${num}" in
+    0)
+        exit 0
+        ;;
+    1)
+        check_uninstall && install
+        ;;
+    2)
+        check_install && update
+        ;;
+    3)
+        check_install && uninstall
+        ;;
+    4)
+        check_install && reset_user
+        ;;
+    5)
+        check_install && reset_config
+        ;;
+    6)
+        check_install && set_port
+        ;;
+    7)
+        check_install && check_config
+        ;;
+    8)
+        check_install && start
+        ;;
+    9)
+        check_install && stop
+        ;;
+    10)
+        check_install && restart
+        ;;
+    11)
+        check_install && status
+        ;;
+    12)
+        check_install && show_log
+        ;;
+    13)
+        check_install && enable
+        ;;
+    14)
+        check_install && disable
+        ;;
+    15)
+        install_bbr
+        ;;
+    16)
+        ssl_cert_issue
+        ;;
+    *)
+        LOGE "Please enter the correct number [0-16]"
+        ;;
+    esac
+}
+
+if [[ $# > 0 ]]; then
+    case $1 in
+    "start")
+        check_install 0 && start 0
+        ;;
+    "stop")
+        check_install 0 && stop 0
+        ;;
+    "restart")
+        check_install 0 && restart 0
+        ;;
+    "status")
+        check_install 0 && status 0
+        ;;
+    "enable")
+        check_install 0 && enable 0
+        ;;
+    "disable")
+        check_install 0 && disable 0
+        ;;
+    "log")
+        check_install 0 && show_log 0
+        ;;
+    "v2-ui")
+        check_install 0 && migrate_v2_ui 0
+        ;;
+    "update")
+        check_install 0 && update 0
+        ;;
+    "install")
+        check_uninstall 0 && install 0
+        ;;
+    "uninstall")
+        check_install 0 && uninstall 0
+        ;;
+    *) show_usage ;;
+    esac
+else
+    show_menu
+fi
